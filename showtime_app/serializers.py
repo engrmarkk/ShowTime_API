39039dcc8ce2039bb4a67ba2b45e23028f8e5e6b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-23 22:06:30 +0100

    user serializer

--- showtime_app/serializers.py
+++ showtime_app/serializers.py
@@ -6,3 +6,9 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
