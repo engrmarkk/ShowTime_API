2c15a804581d0e82567f3cfd0b353112e8ea80b4
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-23 22:04:08 +0100

    Update movie.py

--- showtime_app/endpoints/movie.py
+++ showtime_app/endpoints/movie.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
+from showtime_app.serializers import MovieSerializer
 
 
 class MovieList(generics.ListCreateAPIView):
