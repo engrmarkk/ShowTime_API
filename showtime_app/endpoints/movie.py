89cf623800f7058a52a058ecf7c266452a5bc785
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 12:58:11 +0100

    Update movie.py

--- showtime_app/endpoints/movie.py
+++ showtime_app/endpoints/movie.py
@@ -11,3 +11,13 @@ class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     # permission_classes = (IsAuthenticated,)
+
+
+class MovieDetail(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def get_object(self):
+        title = self.kwargs.get('title')
+        movie = get_object_or_404(Movie, title=title)
+        return movie
