ae125525424ce213a7d6c55dba222557425484c3
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 13:14:42 +0100

    genre url

--- showtime_app/urls.py
+++ showtime_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from endpoints.movie import MovieList, MovieDetail
+from endpoints.movie import MovieList, MovieDetail, MovieGenreList
 
 urlpatterns = [
     path('movies/', MovieList.as_view(), name='movie-list'),
     path('movies/<str:title>/', MovieDetail.as_view(), name='movie-detail'),
+    path('movies/genre/<str:genre>/', MovieGenreList.as_view(), name='movie-genre-list'),
 ]
