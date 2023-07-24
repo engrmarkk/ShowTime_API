088721c1d462c3df782f919c6718c6f33d465ced
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 13:54:56 +0100

    tickets urls

--- showtime_app/urls.py
+++ showtime_app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from endpoints.movie import MovieList, MovieDetail, MovieGenreList, GetMovieByCode
+from endpoints.tickets import GetTicketPriceForAMovie, CreateTicket
 
 urlpatterns = [
+    # Movies
     path('movies/', MovieList.as_view(), name='movie-list'),
     path('movies/<str:title>/', MovieDetail.as_view(), name='movie-detail'),
     path('movies/genre/<str:genre>/', MovieGenreList.as_view(), name='movie-genre-list'),
     path('movies/code/<str:code>/', GetMovieByCode.as_view(), name='movie-code'),
+
+    # Tickets
+    path('tickets/<str:movie_code>/', GetTicketPriceForAMovie.as_view(), name='ticket-price'),
+    path('tickets/create/', CreateTicket.as_view(), name='create-ticket'),
 ]
