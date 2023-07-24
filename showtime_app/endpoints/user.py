8cd38a143182af0a3cf6722295f2dc88277dbe13
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 15:09:54 +0100

    Update user.py

--- showtime_app/endpoints/user.py
+++ showtime_app/endpoints/user.py
@@ -6,9 +6,3 @@ from django.shortcuts import get_object_or_404
 from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
 from showtime_app.serializers import UserSerializer
 
-
-
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    # permission_classes = (IsAuthenticated,)
