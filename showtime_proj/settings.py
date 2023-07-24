37ae619b5d3e5a32bcdb68ee1ef5ff918fa413b2
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 14:42:15 +0100

    Update settings.py

--- showtime_proj/settings.py
+++ showtime_proj/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'ReachOut2Me Social Media API',
-    'DESCRIPTION': 'This is a web application built with Django Restframework that enables users to sign up, log in, and create, edit, and delete posts with customized text, pictures, and links. Users can also like, comment/reply, and save posts, search for other users by username, follow and unfollow users to view their posts, and receive notifications. The application also includes message functionality',
+    'TITLE': 'Show_Time API',
+    'DESCRIPTION': 'This is a web application built with Django Restframework that enables users to book tickets for movies.',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     # OTHER SETTINGS
