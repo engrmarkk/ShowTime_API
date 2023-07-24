9cb660e10bf6cd25b5a896771a4af1e87463c060
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 14:52:18 +0100

    models registration

--- showtime_app/admin.py
+++ showtime_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie, Ticket, Venue, Review, Order, User
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Ticket)
+admin.site.register(Venue)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(User)
