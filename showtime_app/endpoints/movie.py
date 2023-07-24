55aae6ac09294c6e94f1aff4c1e80a96d4dfa62c
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 13:14:01 +0100

    Update movie.py

--- showtime_app/endpoints/movie.py
+++ showtime_app/endpoints/movie.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
+from showtime_app.models import Movie, User
 from showtime_app.serializers import MovieSerializer
 
 
