36a40a964b13cbc9cc257058d723f56f925ecf48
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-24 14:27:07 +0100

    Update models.py

--- showtime_app/models.py
+++ showtime_app/models.py
@@ -47,15 +47,12 @@ class Venue(models.Model):
 
 class Ticket(models.Model):
     movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
-    regular_price = models.DecimalField(max_digits=8, decimal_places=2)
-    vip_price = models.DecimalField(max_digits=8, decimal_places=2)
-    vvip_price = models.DecimalField(max_digits=8, decimal_places=2)
-    # ticket_type = models.CharField(max_length=50, choices=get_ticket_types(), default='regular')
-    # price = models.DecimalField(max_digits=8, decimal_places=2)
+    ticket_type = models.CharField(max_length=50, choices=get_ticket_types(), default='regular')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return self.movie.title
+        return self.ticket_type
 
 
 class Review(models.Model):
