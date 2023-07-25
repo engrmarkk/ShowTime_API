bd9246a7230be8eb01fbd12f3a5015ca20b77914
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-25 15:33:01 +0100

    registration

--- showtime_proj/urls.py
+++ showtime_proj/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     # path('accounts/', include('allauth.urls')),
 
-    # path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
