a720e90072079b818ad6874ecb446579ef828dcc
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-28 18:28:38 +0100

    CustomRegisterView

--- showtime_app/endpoints/user.py
+++ showtime_app/endpoints/user.py
@@ -4,10 +4,26 @@ from rest_framework import generics
 # from rest_framework import status
 # from django.shortcuts import get_object_or_404
 from showtime_app.models import User
-from showtime_app.serializers import UserSerializer
+from showtime_app.serializers import UserSerializer, CustomRegisterSerializer
+from dj_rest_auth.registration.views import RegisterView
 
 
 class ListAllUsers(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [IsAuthenticated]
+
+
+class CustomRegisterView(RegisterView):
+    serializer_class = CustomRegisterSerializer
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     # serializer.is_valid(raise_exception=True)
+    #     # self.perform_create(serializer)
+    #     # headers = self.get_success_headers(serializer.data)
+    #     # return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     return Response({'detail': 'Verification e-mail sent.'}, status=status.HTTP_201_CREATED)
+    #
