41ce298abd24e03a9fda189fb6b7cddee800c80f
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-28 17:43:08 +0100

    Update serializers.py

--- showtime_app/serializers.py
+++ showtime_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
+from rest_framework.exceptions import ValidationError
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -9,9 +10,65 @@ class MovieSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'username', 'is_superuser')
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255)
+    email = serializers.EmailField(max_length=255)
+    password1 = serializers.CharField(max_length=255, write_only=True)
+    password2 = serializers.CharField(max_length=255, write_only=True)
+
+    def validate_email(self, value):
+        qs = User.objects.filter(email__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+
+    def validate_username(self, value):
+        qs = User.objects.filter(username__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
+    def validate_password1(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters")
+        return value
+
+    def validate_passwords(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match")
+        return data
+
+    def get_cleaned_data(self, data):
+        return {
+            'first_name': data.get('first_name', ''),
+            'last_name': data.get('last_name', ''),
+            'username': data.get('username', ''),
+            'email': data.get('email', ''),
+            'password': data.get('password1', ''),
+        }
+
+
+
+    # class Meta:
+    #     model = User
+    #     fields
+
+    def create(self, validated_data):
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
 
 
 class TicketSerializer(serializers.ModelSerializer):
