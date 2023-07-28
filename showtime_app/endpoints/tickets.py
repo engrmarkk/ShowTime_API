88f31803a597aff63619bf700763a1e4d12aab20
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-28 18:39:42 +0100

    updated

--- showtime_app/endpoints/tickets.py
+++ showtime_app/endpoints/tickets.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
+from showtime_app.models import Movie, Venue, Ticket, Review, Order
 from showtime_app.serializers import TicketSerializer
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, OpenApiTypes
 
