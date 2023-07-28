cdfd42b136d7112f4f7de65663da17aa47c38e9b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-28 17:23:10 +0100

    Update user.py

--- showtime_app/endpoints/user.py
+++ showtime_app/endpoints/user.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from showtime_app.models import Movie, Venue, Ticket, Review, Order, User
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.response import Response
+# from rest_framework import status
+# from django.shortcuts import get_object_or_404
+from showtime_app.models import User
 from showtime_app.serializers import UserSerializer
 
+
+class ListAllUsers(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [IsAuthenticated]
