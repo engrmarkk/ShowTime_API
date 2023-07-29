e2ff57cdcfa9e66a3a8a398538c8fd2239f0e0fe
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-29 14:17:29 +0100

    get user url

--- showtime_app/urls.py
+++ showtime_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .endpoints.movie import MovieList, MovieDetail, MovieGenreList, GetMovieByCode
 from .endpoints.tickets import GetTicketPriceForAMovie, CreateTicket
-from .endpoints.user import ListAllUsers, CustomRegisterView, DeleteUser
+from .endpoints.user import ListAllUsers, CustomRegisterView, DeleteUser, GetUser
 
 urlpatterns = [
     # Movies
@@ -12,8 +12,9 @@ urlpatterns = [
 
     # Users
     path('all_users/', ListAllUsers.as_view(), name='user-list'),
-    # path('register/', CustomRegisterView.as_view(), name='register'),
+    path('register/', CustomRegisterView.as_view(), name='register'),
     path('delete_user/<int:pk>/', DeleteUser.as_view(), name='delete-user'),
+    path('user/<int:pk>/', GetUser.as_view(), name='get-user'),
 
     # Tickets
     path('tickets/<str:movie_code>/', GetTicketPriceForAMovie.as_view(), name='ticket-price'),
